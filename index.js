ad45024744b3fe689238f93356a25d4ba8dbb421
ndalilavin <vinson.steve@student.moringaschool.com>
2022-06-19 01:01:06 +0300

    commit tree 2a8ba5b919040159e122fac105e7391d1f066660

---
+++ index.js
@@ -0,0 +1,129 @@
+function standardizeInput(collection) {
+    return (collection instanceof Array) ? collection.slice() : Object.values(collection);
+  }
+  
+  function myEach(collection, callback) {
+    const newCollection = standardizeInput(collection);
+  
+    for (let idx = 0; idx < newCollection.length; idx++) {
+      callback(newCollection[idx]);
+    }
+  
+    return collection;
+  }
+  
+  function myMap(collection, callback) {
+    const newCollection = standardizeInput(collection);
+  
+    const newArr = [];
+  
+    for (let idx = 0; idx < newCollection.length; idx++) {
+      newArr.push(callback(newCollection[idx]));
+    }
+  
+    return newArr;
+  }
+  
+  function myReduce(collection, callback, acc) {
+    let newCollection = standardizeInput(collection);
+  
+    if (!acc) {
+      acc = newCollection[0];
+      newCollection = newCollection.slice(1);
+    }
+  
+    const len = newCollection.length;
+  
+    for (let i = 0; i < len; i++) {
+      acc = callback(acc, newCollection[i], newCollection);
+    }
+    return acc;
+  }
+  
+  function myFind(collection, predicate) {
+    const newCollection = standardizeInput(collection);
+  
+    for (let idx = 0; idx < newCollection.length; idx++) {
+      if (predicate(newCollection[idx])) return newCollection[idx];
+    }
+  
+    return undefined;
+  }
+  
+  function myFilter(collection, predicate) {
+    const newCollection = standardizeInput(collection);
+  
+    const newArr = [];
+  
+    for (let idx = 0; idx < newCollection.length; idx++) {
+      if (predicate(newCollection[idx])) newArr.push(newCollection[idx]);
+    }
+  
+    return newArr;
+  }
+  
+  function mySize(collection) {
+    const newCollection = standardizeInput(collection);
+    return newCollection.length;
+  }
+  
+  function myFirst(arr, stop=false) {
+    return (stop) ? arr.slice(0, stop) : arr[0];
+  }
+  
+  function myLast(arr, start=false) {
+    return (start) ? arr.slice(arr.length-start, arr.length) : arr[arr.length-1];
+  }
+  
+  function mySortBy(arr, callback) {
+    const newArr = [...arr];
+    return newArr.sort(function(a, b) {
+      if (callback(a) > callback(b)) {
+        return 1;
+      } else if (callback(b) > callback(a)) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+  }
+  function unpack(receiver, arr) {
+    for (let val of arr) {
+      receiver.push(val);
+    }
+  }
+  
+  
+  function myFlatten(collection, shallow, newArr=[]) {
+    if (shallow) {
+      for (let val of collection) {
+        Array.isArray(val) ? unpack(newArr, val) : newArr.push(val);
+      }
+    } else {
+      for (let val of collection) {
+        if (Array.isArray(val)) {
+         
+        } else {
+          newArr.push(val);
+        }
+      }
+    }
+    return newArr;
+  }
+  function myKeys(obj) {
+    const keys = [];
+    for (let key in obj){
+      keys.push(key);
+    }
+    return keys;
+  }
+  
+  function myValues(obj) {
+    const values = [];
+    for (let key in obj){
+      values.push(obj[key]);
+    }
+    return values;
+  
+  }
+
